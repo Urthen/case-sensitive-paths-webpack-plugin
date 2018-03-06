91419c52a29f5b27fbde03c82212a339600fa2d9
Simon Kjellberg <simon.kjellberg@gmail.com>
2018-03-06 10:09:29 +0100

    Close webpack watcher in test

--- test/index.js
+++ test/index.js
@@ -114,15 +114,13 @@ describe('CaseSensitivePathsPlugin', () => {
     if (fs.existsSync(testFile)) fs.unlinkSync(testFile);
 
     let compilationCount = 0;
-    compiler.watch({}, (err, stats) => {
+    const watcher = compiler.watch({}, (err, stats) => {
       if (err) done(err);
+
       compilationCount += 1;
-      console.log(compilationCount);
+
       if (compilationCount === 1) {
         const error = stats.toJson().errors[0];
-
-        console.log(stats.hasErrors(), stats.hasWarnings(), stats.toJson());
-        console.dir(stats.toJson());
 
         assert(stats.hasErrors());
         assert(error.indexOf('Cannot resolve') !== -1 || error.indexOf('Can\'t resolve') !== -1);
@@ -130,14 +128,13 @@ describe('CaseSensitivePathsPlugin', () => {
 
         fs.writeFileSync(testFile, 'module.exports = 0;');
       } else if (compilationCount === 2) {
-        console.log(stats.hasErrors(), stats.hasWarnings());
-        console.dir(stats.toJson());
-
         assert(fs.existsSync(testFile), 'Test file should exist');
         assert(!stats.hasErrors(), `Should have no errors, but has: \n${stats.toJson().errors}`);
         assert(!stats.hasWarnings());
+
         fs.unlinkSync(testFile);
-        done();
+
+        watcher.close(done);
       } else {
         throw new Error('Should not reach this point!');
       }
